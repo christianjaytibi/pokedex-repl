e5b0a1ff19fb0d5eccc93202b914af6b3ede3645
christianjaytibi <christianjaytibi@gmail.com>
2026-09-13 21:19:39 +0800

    feat: implement REPL loop

--- repl.go
+++ repl.go
@@ -1,7 +1,32 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading input:", err)
+		}
+
+		input := scanner.Text()
+		words := cleanInput(input)
+		if len(words) == 0 {
+			continue
+		}
+
+		command := words[0]
+		fmt.Printf("Your command was: %s\n", command)
+	}
+}
 func cleanInput(text string) []string {
 	lowered := strings.ToLower(text)
 	return strings.Fields(lowered)
